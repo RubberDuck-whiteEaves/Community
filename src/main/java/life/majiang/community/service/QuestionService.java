020068a3a6188f6c7b56320c72b3835959ff4934
RubberDuck-whiteEaves <838106224@qq.com>
2022-10-25 18:46:35 +0800

    complete update questions function

--- src/main/java/life/majiang/community/service/QuestionService.java
+++ src/main/java/life/majiang/community/service/QuestionService.java
@@ -93,4 +93,16 @@ public class QuestionService {
         questionDTO.setUser(userMapper.findByID(questionDTO.getCreator()));
         return questionDTO;
     }
+
+    public void createOrUpdate(Question question) {
+        if(question.getId()==null){
+            question.setGmtCreate(System.currentTimeMillis());
+            question.setGmtModified(question.getGmtCreate());
+            questionMapper.create(question);
+        }
+        else{
+            question.setGmtModified(System.currentTimeMillis());
+            questionMapper.update(question);
+        }
+    }
 }
